251269cd6a148dff64dbb24086b68f4fb2979e31
Flavien G <44913995+fgaujard@users.noreply.github.com>
2023-11-29 17:27:03 +0100

    all passed

--- tests/movies.test.js
+++ tests/movies.test.js
@@ -3,18 +3,20 @@ const app = require("../app");
 
 describe("GET /api/movies", () => {
   it("should return all movies", async () => {
-    /** Code your test here */
     const response = await request(app).get("/api/movies");
+
     expect(response.headers["content-type"]).toMatch(/json/);
+
     expect(response.status).toEqual(200);
   });
 });
 
 describe("GET /api/movies/:id", () => {
   it("should return one movie", async () => {
-    /** Code your test here */
-    const response = await request(app).get("/api/movies").send("1");
+    const response = await request(app).get("/api/movies/1");
+
     expect(response.headers["content-type"]).toMatch(/json/);
+
     expect(response.status).toEqual(200);
   });
 });
