a92851da5427a7b1a059a20c615d08ba5a766a30
Flavien G <44913995+fgaujard@users.noreply.github.com>
2023-11-29 17:30:11 +0100

    bonus

--- tests/movies.test.js
+++ tests/movies.test.js
@@ -2,7 +2,7 @@ const request = require("supertest");
 const app = require("../app");
 
 describe("GET /api/movies", () => {
-  it("should return all movies", async () => {
+  it("should return all movies (200)", async () => {
     const response = await request(app).get("/api/movies");
 
     expect(response.headers["content-type"]).toMatch(/json/);
@@ -12,11 +12,16 @@ describe("GET /api/movies", () => {
 });
 
 describe("GET /api/movies/:id", () => {
-  it("should return one movie", async () => {
+  it("should return one movie (200)", async () => {
     const response = await request(app).get("/api/movies/1");
 
     expect(response.headers["content-type"]).toMatch(/json/);
 
     expect(response.status).toEqual(200);
+  });
+  it("should return no movie (404)", async () => {
+    const response = await request(app).get("/api/movies/0");
+
+    expect(response.status).toEqual(404);
   });
 });
