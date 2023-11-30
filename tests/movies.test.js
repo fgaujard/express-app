4ca515aba17ae8d11d9c1fc409c1057984bbc2e4
Flavien G <44913995+fgaujard@users.noreply.github.com>
2023-11-30 15:34:49 +0100

    quest_2.1

--- tests/movies.test.js
+++ tests/movies.test.js
@@ -1,10 +1,10 @@
 const request = require("supertest");
-
 const app = require("../src/app");
-
 const database = require("../database");
 
 afterAll(() => database.end());
+
+// Verify if there is all movies access
 
 describe("GET /api/movies", () => {
   it("should return all movies", async () => {
@@ -15,6 +15,8 @@ describe("GET /api/movies", () => {
     expect(response.status).toEqual(200);
   });
 });
+
+// Verify if there is one movie
 
 describe("GET /api/movies/:id", () => {
   it("should return one movie", async () => {
@@ -29,5 +31,50 @@ describe("GET /api/movies/:id", () => {
     const response = await request(app).get("/api/movies/0");
 
     expect(response.status).toEqual(404);
+  });
+});
+
+// Verify if can add movie
+
+describe("POST /api/movies", () => {
+  it("should return created movie", async () => {
+    const newMovie = {
+      title: "Star Wars",
+      director: "George Lucas",
+      year: "1977",
+      color: "1",
+      duration: 120,
+    };
+
+    // if can add
+    const response = await request(app).post("/api/movies").send(newMovie);
+
+    expect(response.status).toEqual(201);
+    expect(response.body).toHaveProperty("id");
+    expect(typeof response.body.id).toBe("number");
+
+    const [result] = await database.query(
+      "SELECT * FROM movies WHERE id=?",
+      response.body.id
+    );
+
+    // if is add
+    const [movieInDatabase] = result;
+
+    expect(movieInDatabase).toHaveProperty("id");
+    expect(movieInDatabase).toHaveProperty("title");
+    expect(movieInDatabase.title).toStrictEqual(newMovie.title);
+  });
+
+  // if missing props
+
+  it("should return an error", async () => {
+    const movieWithMissingProps = { title: "Harry Potter" };
+
+    const response = await request(app)
+      .post("/api/movies")
+      .send(movieWithMissingProps);
+
+    expect(response.status).toEqual(500);
   });
 });
--- tests/users.test.js
+++ tests/users.test.js
@@ -1,10 +1,11 @@
 const request = require("supertest");
-
 const app = require("../src/app");
-
 const database = require("../database");
+const crypto = require("node:crypto");
 
 afterAll(() => database.end());
+
+// Verify if there is all users access
 
 describe("GET /api/users", () => {
   it("should return all users", async () => {
@@ -15,6 +16,8 @@ describe("GET /api/users", () => {
     expect(response.status).toEqual(200);
   });
 });
+
+// Verify if there is one user
 
 describe("GET /api/users/:id", () => {
   it("should return one user", async () => {
@@ -29,5 +32,48 @@ describe("GET /api/users/:id", () => {
     const response = await request(app).get("/api/users/0");
 
     expect(response.status).toEqual(404);
+  });
+});
+
+// Verify if can add user
+
+describe("POST /api/users", () => {
+  it("should return created user", async () => {
+    const newUser = {
+      firstname: "Marie",
+      lastname: "Martin",
+      email: `${crypto.randomUUID()}@wild.co`,
+      city: "Paris",
+      language: "French",
+    };
+    // if can add
+    const response = await request(app).post("/api/users").send(newUser);
+
+    expect(response.status).toEqual(201);
+    expect(response.body).toHaveProperty("id");
+    expect(typeof response.body.id).toBe("number");
+
+    const [result] = await database.query(
+      "SELECT * FROM users WHERE id=?",
+      response.body.id
+    );
+    // if is add
+    const [userInDatabase] = result;
+
+    expect(userInDatabase).toHaveProperty("id");
+    expect(userInDatabase).toHaveProperty("firstname");
+    expect(userInDatabase.firstname).toStrictEqual(newUser.firstname);
+  });
+
+  // if missing props
+
+  it("should return an error", async () => {
+    const userWithMissingProps = { firstname: "Harry" };
+
+    const response = await request(app)
+      .post("/api/users")
+      .send(userWithMissingProps);
+
+    expect(response.status).toEqual(500);
   });
 });
